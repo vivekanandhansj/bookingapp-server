6e1d2a3df460194d9ab6ac71af14082f4fb73784
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-09-16 07:06:43 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,9 @@ router.get("/checkauthentication", verifyToken, (req,res,next)=>{
 // router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
 //   res.send("hello user, you are logged in and you can delete your account")
 // })
-
 // router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
 //   res.send("hello admin, you are logged in and you can delete all accounts")
 // })
-
 //UPDATE
 router.put("/:id",verifyUser,updateUser);
  
