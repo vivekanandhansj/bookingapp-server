9e3f33f6609620ca0c52fa262bdc8a4da4261e37
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-09-16 07:04:37 +0100

    Update hotels.js

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,23 +3,17 @@ import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotelR
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
-
 const router =express.Router();
 //CREATE
 router.post("/",verifyAdmin, createHotel)
-
 //UPDATE
 router.put("/:id",verifyAdmin, updateHotel)
-
 //DELETE
 router.delete("/:id",verifyAdmin, deleteHotel)
-
 //GET
 router.get("/find/:id",getHotel)
-
 //GETALL
 router.get("/", getHotels)
-
 router.get("/countByCity",countByCity);
 router.get("/countByType",countByType);
 router.get("/room/:id",getHotelRooms);
